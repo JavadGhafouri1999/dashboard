bd25d2faa64c5eef9b8c2817137abca43b88a6cd
Javad-code-stack <javadghafouri1999@gmail.com>
2025-08-24 22:33:30 +0330

    Admin Dashboard Sidebar - Completed Navbar - Completed

---
+++ src/components/AppSidebar.tsx
@@ -0,0 +1,217 @@
+import {
+	Calendar,
+	ChevronDown,
+	ChevronUp,
+	DollarSign,
+	Home,
+	Inbox,
+	LogOut,
+	NotebookTabs,
+	Plus,
+	Projector,
+	Search,
+	Settings,
+	User2,
+	UserRound,
+} from "lucide-react";
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarFooter,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarHeader,
+	SidebarMenu,
+	SidebarMenuBadge,
+	SidebarMenuButton,
+	SidebarMenuItem,
+	SidebarMenuSub,
+	SidebarMenuSubButton,
+	SidebarMenuSubItem,
+	SidebarSeparator,
+} from "./ui/sidebar";
+import Link from "next/link";
+import Image from "next/image";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Collapsible, CollapsibleContent, CollapsibleTrigger } from "./ui/collapsible";
+
+const items = [
+	{ title: "خانه", url: "/", icon: Home },
+	{ title: "پیامها", url: "#", icon: Inbox },
+	{ title: "تقویم", url: "#", icon: Calendar },
+	{ title: "جستجو", url: "#", icon: Search },
+	{ title: "تنظیمات", url: "#", icon: Settings },
+];
+
+const AppSidebar = () => {
+	return (
+		<Sidebar collapsible="icon" side="right">
+			<SidebarHeader className="py-3">
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton>
+							<Link href="/" className="w-full flex items-center justify-between gap-4">
+								<Image
+									src="/logo.svg"
+									alt="logo"
+									width={30}
+									height={30}
+									className="text-white stroke-amber-50 fill-amber-100"
+								/>
+								<span className="font-semibold text-lg">BrainCog</span>
+							</Link>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarSeparator></SidebarSeparator>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>منوی کاربری</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<Link href={item.url}>
+											<item.icon />
+											{item.title}
+										</Link>
+									</SidebarMenuButton>
+									{item.title === "پیامها" && (
+										<SidebarMenuBadge className="border border-red-500">
+											99+
+										</SidebarMenuBadge>
+									)}
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+				<SidebarGroup>
+					<SidebarGroupLabel>پروژه ها</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Link href="#">
+										<Projector />
+										<span>تمام پروژه ها</span>
+									</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Link href="#">
+										<Plus />
+										<span>پروژه جدید</span>
+									</Link>
+								</SidebarMenuButton>
+							</SidebarMenuItem>
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+				{/* Collapsible */}
+				<Collapsible defaultOpen className="group/collapsible">
+					<SidebarGroup>
+						<SidebarGroupLabel asChild>
+							<CollapsibleTrigger>
+								اطلاعات حساب
+								<ChevronDown className="mr-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+							</CollapsibleTrigger>
+						</SidebarGroupLabel>
+						<CollapsibleContent>
+							<SidebarGroupContent>
+								<SidebarMenu>
+									<SidebarMenuItem>
+										<SidebarMenuButton asChild>
+											<Link href="#">
+												<DollarSign />
+												<span>امور مالی</span>
+											</Link>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+									<SidebarMenuItem>
+										<SidebarMenuButton asChild>
+											<Link href="#">
+												<NotebookTabs />
+												<span>جزییات حساب</span>
+											</Link>
+										</SidebarMenuButton>
+									</SidebarMenuItem>
+								</SidebarMenu>
+							</SidebarGroupContent>
+						</CollapsibleContent>
+					</SidebarGroup>
+				</Collapsible>
+
+				{/* Nested */}
+				<SidebarGroup>
+					<SidebarGroupLabel>جزییات پروژه</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							<SidebarMenuItem>
+								<SidebarMenuButton asChild>
+									<Link href="#">
+										<Projector />
+										<span>تمام پروژه ها</span>
+									</Link>
+								</SidebarMenuButton>
+								<SidebarMenuSub>
+									<SidebarMenuSubItem>
+										<SidebarMenuSubButton asChild>
+											<Link href="#">
+												<Plus />
+												تغیرات اخیر
+											</Link>
+										</SidebarMenuSubButton>
+									</SidebarMenuSubItem>
+									<SidebarMenuSubItem>
+										<SidebarMenuSubButton asChild>
+											<Link href="#">
+												<Plus />
+												تغیرات اخیر
+											</Link>
+										</SidebarMenuSubButton>
+									</SidebarMenuSubItem>
+								</SidebarMenuSub>
+							</SidebarMenuItem>
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<DropdownMenu>
+							<DropdownMenuTrigger asChild>
+								<SidebarMenuButton className="flex items-center">
+									<User2 />
+									<span className="mt-1">User_1</span>
+									<ChevronUp className="mr-auto" />
+								</SidebarMenuButton>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent align="start" className="rtlDir ml-4">
+								<DropdownMenuItem>
+									<UserRound />
+									پروفایل
+								</DropdownMenuItem>
+								<DropdownMenuItem>
+									<Settings />
+									تنظیمات
+								</DropdownMenuItem>
+								<DropdownMenuItem variant="destructive">
+									<LogOut />
+									خروج
+								</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+		</Sidebar>
+	);
+};
+
+export default AppSidebar;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { LogOut, Moon, Settings, Sun, SunMoon, UserRound } from "lucide-react";
+import Link from "next/link";
+import { Button } from "./ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useTheme } from "next-themes";
+import { SidebarTrigger } from "./ui/sidebar";
+
+const Navbar = () => {
+	const { setTheme } = useTheme();
+	return (
+		<nav className="p-4 flex items-center justify-between gap-2 rtlDir">
+			{/* Left */}
+			<Button size="sm" variant="outline" className="cursor-pointer" asChild>
+				<SidebarTrigger />
+			</Button>
+			{/* Right */}
+			<div className="flex items-center gap-4">
+				<Link href="/">Dashboard</Link>
+
+				{/* Theme Switch */}
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant="outline" size="icon">
+							<Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+							<Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+							<span className="sr-only">Toggle theme</span>
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent align="end" className="rtlDir ml-4">
+						<DropdownMenuItem onClick={() => setTheme("light")}>
+							<Sun />
+							روشن
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme("dark")}>
+							<Moon />
+							تاریک
+						</DropdownMenuItem>
+						<DropdownMenuItem onClick={() => setTheme("system")}>
+							<SunMoon />
+							سیستم
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+
+				{/* User Menu */}
+				<DropdownMenu>
+					<DropdownMenuTrigger>
+						<Avatar className="cursor-pointer">
+							<AvatarImage src="https://github.com/shadcn.png" />
+							<AvatarFallback>CN</AvatarFallback>
+						</Avatar>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent className="rtlDir ml-4">
+						<DropdownMenuLabel>حساب من</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem>
+							<UserRound />
+							پروفایل
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<Settings />
+							تنظیمات
+						</DropdownMenuItem>
+						<DropdownMenuItem variant="destructive">
+							<LogOut />
+							خروج
+						</DropdownMenuItem>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
